46b6e1023fed9af31b85ed253207bc1ee35c1022
Eric <epasho@yahoo.com>
2023-12-02 01:18:44 -0500

    day 2

--- 2/part2.cr
+++ 2/part2.cr
@@ -1,11 +1,9 @@
 #advent of code day 2 part 2
 
 sum = 0
-lastGame = 0
 File.each_line("input") do |line|
   #get the game number part of the line
   game = line.split(':')[0].split(' ')[1]
-  counted = false
 
   #seperate the 'hands'
   hands = line.split(/[,;:]/)
@@ -19,11 +17,8 @@ File.each_line("input") do |line|
     hand = hand.split(' ')
     hand = hand.reject { |ele| ele.empty? }
 
-    #get the most recent game number
-    if hand[0] == "Game"
-      lastGame = hand[1].to_i
     #find the greatest number for each color
-    elsif hand[1] == "red" && hand[0].to_i > redMax
+    if hand[1] == "red" && hand[0].to_i > redMax
       redMax = hand[0].to_i if redMax < hand[0].to_i
     elsif hand[1] == "blue" && hand[0].to_i > blueMax
       blueMax = hand[0].to_i if blueMax < hand[0].to_i
